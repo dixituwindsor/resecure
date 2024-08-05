8a8392f98259ea5022e2cdc6a99a6a5bf64cf263
dixituwindsor <135763503+dixituwindsor@users.noreply.github.com>
2024-08-05 13:12:33 -0400

    added code of both the projects

---
+++ resecure_camera/lib/publisher.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:millicast_flutter_sdk/millicast_flutter_sdk.dart';
+
+Future publishConnect(RTCVideoRenderer localRenderer, String streamName,
+    bool isBackCamera) async {
+  // Setting subscriber options
+  DirectorPublisherOptions directorPublisherOptions = DirectorPublisherOptions(
+      token: '',
+      streamName: streamName);
+
+  /// Define callback for generate new token
+  tokenGenerator() => Director.getPublisher(directorPublisherOptions);
+  print("Dbg stream name: $streamName");
+
+  /// Create a new instance
+  Publish publish =
+      Publish(streamName: streamName, tokenGenerator: tokenGenerator);
+
+  final Map<String, dynamic> constraints = <String, dynamic>{
+    'audio': false,
+    'video': {
+      'facingMode': isBackCamera ? 'environment' : 'user', // Or 'environment'
+    },
+  };
+
+  MediaStream stream = await navigator.mediaDevices.getUserMedia(constraints);
+  localRenderer.srcObject = stream;
+
+  //Publishing Options
+  Map<String, dynamic> broadcastOptions = {'mediaStream': stream};
+  //Some Android devices do not support h264 codec for publishing
+  if (Platform.isAndroid) {
+    broadcastOptions['codec'] = 'vp8';
+  }
+
+  /// Start connection to publisher
+  try {
+    await publish.connect(options: broadcastOptions);
+    return publish;
+  } catch (e) {
+    throw Exception(e);
+  }
+}
---
+++ resecure_user/lib/send_email.dart
@@ -0,0 +1,31 @@
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+
+class EmailService {
+  static const String _username =
+      ''; // Replace with your Elastic Email API key
+  static const String _password =
+      ''; // Replace with your Elastic Email API key
+
+  Future<void> sendEmail(String toEmail, String subject, String body) async {
+    final smtpServer = SmtpServer(
+      'smtp.elasticemail.com',
+      port: 2525,
+      username: _username,
+      password: _password,
+    );
+
+    final message = Message()
+      ..from = const Address(_username, 'ReSecure')
+      ..recipients.add(toEmail)
+      ..subject = subject
+      ..html = body;
+
+    try {
+      final sendReport = await send(message, smtpServer);
+      print('Email sent: ${sendReport.toString()}');
+    } catch (e) {
+      print('Error sending email: $e');
+    }
+  }
+}
